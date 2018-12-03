d066190645260d8d5113e8b0c48a30107b73c537
Ivansk27 <Ivan2787.28@ukr.net>
2018-12-03 22:45:45 +0200

    ivan 2nd commit

--- for-loop/task.js
+++ for-loop/task.js
@@ -1,9 +1,17 @@
 function listAll(min, max) {
-  var result = '1 2 3 ';
+  var result = " ";
+  for (var a = min; a <= max; a++) {
+    result = result + a + " ";
+  }
   return result.trim();
 }
 
 function listBetweenDesc(min, max) {
-  var result = '3 2';
+  var result = "";
+  // max = max - 1;
+  for (var a = max - 1; a> min; a--) {
+    result = result + a + " ";
+  }
   return result.trim();
 }
+
